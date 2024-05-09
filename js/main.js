43318a882d6b7598a1ff570c2c3ba46bdc64fea2
Rachele-Turtora <racheleturtora21@gmail.com>
2024-05-09 18:45:17 +0200

    [UPD] milestone 3

--- js/main.js
+++ js/main.js
@@ -1,33 +1,65 @@
 "use strict";
 
+const display = document.getElementById("result");
+let num1;
+let num2;
+let operator = "";
+
 const numbers = document.querySelectorAll(".number");
 
 // Adding EventListener to all the numbers
 for (let i = 0; i < numbers.length; i++){
-    numbers[i].addEventListener("click", selectNumber);
+    numbers[i].addEventListener("click", function (){
+        selectNumber(numbers[i].innerText)
+    });
 }
 
 // Taking the number an putting it in the result
-function selectNumber(){
-    const numberValue = this.innerText;
+function selectNumber(selectedNumber){
     
     const result = document.getElementById("result");
-    result.innerText = numberValue;
+    if (result.innerText == "0"){
+        result.innerText = selectedNumber;
+    } else {
+        result.innerText += selectedNumber;
+    }
 }
 
 
-const operators = document.querySelectorAll(".operator")
+const operators = document.querySelectorAll(".operator");
 
 // Adding EventListener to all the operators
 for (let i = 0; i < operators.length; i++){
-    operators[i]. addEventListener("click", selectOperator);
+    operators[i]. addEventListener("click", function (){
+        // Taking the operator and the first number, then resetting the result
+        num1 = parseInt(display.innerText);
+        display.innerText = "";
+        selectOperator(operators[i].innerText);
+    });
 }
 
-// Taking the operator and the first number, then resetting the result
-function selectOperator(){
-    const operatorValue = this.innerText;
-    let result = document.getElementById("result");
-    console.log(operatorValue);
-    console.log(result.innerText);
-    result.innerHTML = "";
+function selectOperator(selectedOperator){
+    operator = selectedOperator;
+}
+
+const equal = document.getElementById("equal");
+
+// Taking the second number
+equal.addEventListener("click", function(){
+    num2 = parseInt(display.innerText);
+    display.innerText = operation();
+})
+
+// Calculating
+function operation(){
+    switch (operator){
+        case "+":
+            return num1 + num2
+        case "-":
+            return num1 - num2
+        case "x":
+            return num1 * num2
+        case "/":
+            return num1 / num2
+   }
 }
